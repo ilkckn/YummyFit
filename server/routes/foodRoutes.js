babf2a3d9c9d5ed6882ab99f5b0b854265ba396a
Abdulwahed Bin Mothana <abdulwahed.mothana@yahoo.com>
2025-04-25 10:20:35 +0200

    router

---
+++ server/routes/foodRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getFoods,
+  getFoodById,
+  createFood,
+  updateFood,
+  deleteFood,
+} from "../controllers/foodController.js";
+
+const router = express.Router();
+//aa 
+router.get("/", getFoods);
+router.get("/:id", getFoodById);
+router.post("/", createFood);
+router.put("/:id", updateFood);
+router.delete("/:id", deleteFood);
+
+export default router;
